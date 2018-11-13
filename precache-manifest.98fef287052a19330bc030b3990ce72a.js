9bf3aa9afd33f62b7a4736934c740eb9636fc6a4
Coffee Warrior <lionlikelucas@gmail.com>
2018-11-13 00:11:47 -0500

    Updates

---
+++ precache-manifest.98fef287052a19330bc030b3990ce72a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c382919265074b06e9f",
+    "url": "/support-page-joatspace/static/js/runtime~main.3c382919.js"
+  },
+  {
+    "revision": "c4a52af06b74cf6ecf93",
+    "url": "/support-page-joatspace/static/js/main.c4a52af0.chunk.js"
+  },
+  {
+    "revision": "1456546a39e7f2e10890",
+    "url": "/support-page-joatspace/static/js/1.1456546a.chunk.js"
+  },
+  {
+    "revision": "5ace291e017fd78e253d4ed98d5a70a9",
+    "url": "/support-page-joatspace/index.html"
+  }
+];
