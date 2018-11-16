8935f0bc54ba086fd6356fb60af32dbbbb41a4db
Coffee Warrior <lionlikelucas@gmail.com>
2018-11-15 21:43:38 -0500

    added axios and posting to firebase server

--- src/stateful/Form.js
+++ src/stateful/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormElement from "../stateless/formElement"
 import Button from "../stateless/button"
+import axios from "../helpers/axios"
 
 class Form extends Component {
   state = {
@@ -11,6 +12,7 @@ class Form extends Component {
   //function will update state based off changes to form element
   //accepts subject as a bool value to update either the subject or the body property
   handleFormElementChange = (event) => {
+    event.preventDefault()
     const currentState = {...this.state}
     
     if(event.currentTarget.name == "subject"){
@@ -23,12 +25,25 @@ class Form extends Component {
     this.setState({...currentState})
   }
 
+  submitForm = (e) => {
+    e.preventDefault()
+    const form = {
+      subject: this.state.subject,
+      body: this.state.body
+    } 
+    
+    
+    axios.post('/supportForms.json', form)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
+
   render() {
     //set background
     
     //form needs 2 inputs, the subject and body. The FormElements accept 2 elements, the rows, and the width in percent
     return (
-        <form action={`mailto:lionlikelucas@gmail.com?subject=${this.state.subject}`}>
+        <form onSubmit={this.submitForm}>
         <FormElement rows="1" width="50" value={this.state.subject} name="subject" onChange={(e) => this.handleFormElementChange(e)}>subject</FormElement>
         <FormElement rows="7" width="50" value={this.state.body} name="body" onChange={(e) => this.handleFormElementChange(e)}>body</FormElement>
         
